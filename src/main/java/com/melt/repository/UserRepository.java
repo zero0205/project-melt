5137f929ca6bccc1efc977d84ff2f7db672fe2f0
zero0205 <etety39@naver.com>
2025-09-02 01:14:11 +0900

    [4주차] 테스트용 클래스 구현

--- src/main/java/com/melt/repository/UserRepository.java
+++ src/main/java/com/melt/repository/UserRepository.java
@@ -4,7 +4,7 @@ import com.melt.annotation.Repository;
 
 @Repository
 public class UserRepository {
-    public void save() {
-        System.out.println("사용자 저장 완료!");
+    public void save(String user) {
+        System.out.println("UserRepository: " + user + "저장됨");
     }
 }
--- src/main/java/com/melt/service/UserService.java
+++ src/main/java/com/melt/service/UserService.java
@@ -1,10 +1,21 @@
 package com.melt.service;
 
+import com.melt.annotation.Autowired;
 import com.melt.annotation.Service;
+import com.melt.repository.UserRepository;
 
 @Service
 public class UserService {
-    public void processUser() {
-        System.out.println("사용자 처리 중...");
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveUser(String user) {
+        System.out.println("UserService: 사용자 저장 요청");
+        userRepository.save(user);
+    }
+
+    // DI 잘 됐는지 확인용
+    public void checkDependency() {
+        System.out.println("UserRepository 주입 상태: " + (userRepository != null));
     }
 }
